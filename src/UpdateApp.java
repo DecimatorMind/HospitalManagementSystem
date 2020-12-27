52ecfa73ef646e84c1a5e76040a796b9aa753607
Pranjal Bhardwaj <54343538+DecimatorMind@users.noreply.github.com>
2020-12-27 21:48:44 +0530

    Update UpdateApp.java

--- src/UpdateApp.java
+++ src/UpdateApp.java
@@ -181,7 +181,7 @@ public class UpdateApp extends JFrame {
 				try {
 					Class.forName("com.mysql.jdbc.Driver");
 					java.sql.Connection conn;
-					conn = DriverManager.getConnection("jdbc:mysql://localhost/Project?autoReconnect=true&useSSL=false", "root", "Pranjal1");
+					conn = DriverManager.getConnection("jdbc:mysql://localhost/Project?autoReconnect=true&useSSL=false", "root", "root");
 					Statement stmt = conn.createStatement();
 					String sql_1 = "Select appointmentid from appointment";
 					ResultSet result = stmt.executeQuery(sql_1);
